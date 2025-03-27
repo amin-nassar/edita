276b64cbf7c9d273aec36e5fd647b88bfe142d82
amin-nassar <amin.nassar.ce@gmail.com>
2025-03-27 23:15:41 +0200

    Added Tailwind configs and basic styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <h1 className="text-7xl text-teal-600 font-bold underline">
-      Welcome to Edita
-    </h1>
-  );
+  return <h1 className="text-7xl font-bold underline">Welcome to Edita</h1>;
 }
 
 export default App;
