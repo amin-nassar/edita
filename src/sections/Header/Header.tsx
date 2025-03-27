fa585091055c66fe396d1fa1098e30a84d53c601
amin-nassar <amin.nassar.ce@gmail.com>
2025-03-28 01:58:03 +0200

    Show/Hide header on small devices based on menu button click.

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import NavLink from "./NavLink";
 
 const Header = () => {
-  const dot = <div className="size-1.5 rounded-full bg-p2 max-lg:hidden" />;
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleOpen = () => setIsOpen((prev) => !prev);
+
+  const dot = <div className="size-1.5 rounded-full bg-p2 max-lg:hidden" />;
   return (
     <header className="fixed top-0 left-0 z-50 w-full py-10">
       <div className="mx-auto max-w-[1252px] px-16 max-xl:px-10 max-lg:px-6 max-sm:px-4 flex h-14 items-center ">
@@ -10,7 +14,11 @@ const Header = () => {
           <img src="/images/edita.svg" width={115} height={55} alt="Logo" />
         </a>
 
-        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+        <div
+          className={`w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 ${
+            isOpen ? "max-lg:opacity-0" : ""
+          }`}
+        >
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden max-md:px-4 sidebar-before">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
@@ -38,6 +46,25 @@ const Header = () => {
             </nav>
           </div>
         </div>
+
+        <button
+          className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
+          onClick={toggleOpen}
+        >
+          {isOpen ? (
+            <img
+              src="/images/magic.svg"
+              alt="Open"
+              className="size-1/2 object-contain"
+            />
+          ) : (
+            <img
+              src="/images/close.svg"
+              alt="Close"
+              className="size-1/2 object-contain"
+            />
+          )}
+        </button>
       </div>
     </header>
   );
