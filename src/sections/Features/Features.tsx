841061075cdd41691a98e9d5d8b75b21059c9e68
amin-nassar <amin.nassar.ce@gmail.com>
2025-03-30 00:51:21 +0200

    Added key to features list mapping.

--- src/sections/Features/Features.tsx
+++ src/sections/Features/Features.tsx
@@ -8,7 +8,7 @@ const Features = () => {
       <div className="mx-auto max-w-[1252px] px-16 max-xl:px-10 max-lg:px-6 max-sm:px-4">
         <div className="relative flex flex-nowrap md:flex-wrap border-2 border-s2 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g7 max-md:border-none max-md:rounded-none max-md:gap-3">
           {features.map((feature) => (
-            <Feature feature={feature} />
+            <Feature key={feature.id} feature={feature} />
           ))}
 
           <Details details={details} />
