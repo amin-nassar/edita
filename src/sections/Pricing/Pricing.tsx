8a401e5af863210ac811e53ec3a00b778069422e
amin-nassar <amin.nassar.ce@gmail.com>
2025-03-30 03:11:35 +0200

    Animated plans' prices on changing plan types using `react-countup` package`

--- src/sections/Pricing/Pricing.tsx
+++ src/sections/Pricing/Pricing.tsx
@@ -3,6 +3,7 @@ import PricingButton from "./PricingButton";
 import PricingBackground from "./PricingBackground";
 import { plans } from "../../constants";
 import Button from "../../components/Button";
+import CountUp from "react-countup";
 
 const Pricing = () => {
   const [isMonthlyPlan, setIsMonthlyPlan] = useState(true);
@@ -37,37 +38,40 @@ const Pricing = () => {
 
         <div className="scroll-hide relative z-2 -mt-12 flex items-start justify-start max-lg:gap-3 max-md:gap-5 max-xl:overflow-auto max-xl:pt-6">
           {plans.map((plan, index) => {
+            const { priceMonthly, priceYearly } = plan;
+            const isPrimary = index === 1;
+
             return (
               <div
                 key={plan.id}
                 className="pricing-plan_first pricing-plan_last pricing-plan_even pricing-plan_odd relative border-2 p-7 max-xl:min-w-72 max-lg:rounded-3xl flex-grow"
               >
-                {index === 1 && (
+                {isPrimary && (
                   <div className="g4 absolute h-330 left-0 right-0 top-0 z-1 rounded-t-3xl" />
                 )}
 
                 <div
                   className={`absolute left-0 right-0 z-2 flex items-center justify-center -top-6 ${
-                    index === 1 ? "" : "xl:-top-11"
+                    isPrimary ? "" : "xl:-top-11"
                   }`}
                 >
                   <img
                     src={plan.logo}
                     alt={plan.title}
                     className={`object-contain drop-shadow-2xl ${
-                      index === 1 ? "size-[120px]" : "size-[88px]"
+                      isPrimary ? "size-[120px]" : "size-[88px]"
                     }`}
                   />
                 </div>
 
                 <div
                   className={`relative flex flex-col items-center ${
-                    index === 1 ? "pt-24" : "pt-12"
+                    isPrimary ? "pt-24" : "pt-12"
                   }`}
                 >
                   <p
                     className={`text-[12px] font-bold leading-[16px] tracking-[0.3em] rounded-20 relative z-2 mx-auto mb-6 border-2 px-4 py-1.5 uppercase ${
-                      index === 1 ? "border-p3 text-p3" : "border-p1 text-p1"
+                      isPrimary ? "border-p3 text-p3" : "border-p1 text-p1"
                     }`}
                   >
                     {plan.title}
@@ -76,10 +80,16 @@ const Pricing = () => {
                   <div className="relative z-2 flex items-center justify-center">
                     <div
                       className={`font-inter text-[72px] font-bold leading-[84px] flex items-start ${
-                        index === 1 ? "text-p3" : "text-p4"
+                        isPrimary ? "text-p3" : "text-p4"
                       }`}
                     >
-                      ${isMonthlyPlan ? plan.priceMonthly : plan.priceYearly}
+                      <CountUp
+                        start={isMonthlyPlan ? priceYearly : priceMonthly}
+                        end={isMonthlyPlan ? priceMonthly : priceYearly}
+                        duration={0.4}
+                        useEasing
+                        prefix="$"
+                      />
                     </div>
                     <div className="text-[14px] font-semibold leading-[18px] tracking-[0.03em] relative top-3 ml-1 uppercase">
                       / mo
@@ -88,7 +98,7 @@ const Pricing = () => {
                 </div>
                 <div
                   className={`text-[22px] leading-[36px] relative z-2 mb-10 w-full border-b-s2 pb-9 text-p4 text-center ${
-                    index === 1 ? "border-b" : ""
+                    isPrimary ? "border-b" : ""
                   }`}
                 >
                   {plan.caption}
@@ -115,7 +125,7 @@ const Pricing = () => {
                   <Button icon={plan.icon}>Get Started</Button>
                 </div>
 
-                {index === 1 && (
+                {isPrimary && (
                   <p className="mt-9 text-center text-[12px] font-semibold leading-[18px] tracking-[0.03em] text-p3 before:content-['-'] before:mx-2.5 after:content-['-'] after:mx-2.5">
                     limited time offer
                   </p>
