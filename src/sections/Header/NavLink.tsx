016746b0a11a401e6b465cf2258cb73e24534f04
amin-nassar <amin.nassar.ce@gmail.com>
2025-04-01 20:29:40 +0200

    Added highlight for current active nav link.

--- src/sections/Header/NavLink.tsx
+++ src/sections/Header/NavLink.tsx
@@ -9,6 +9,8 @@ const NavLink = ({ title }: NavLinkProps) => {
     <Link
       to={title}
       smooth
+      spy
+      activeClass="!text-p3"
       offset={-100}
       className="text-[16px] font-bold leading-[24px] text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
     >
